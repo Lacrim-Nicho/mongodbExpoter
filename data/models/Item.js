c43777ae0934e9378b397935a1f71aaf8131d0ba
Nicholas. <113456464+Lacrim-Nicho@users.noreply.github.com>
2025-05-30 18:29:06 +0200

    Create Item.js

---
+++ data/models/Item.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item; 
